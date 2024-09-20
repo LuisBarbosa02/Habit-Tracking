5aeb63f41ba2a7f86ae3274b9798fbd4785b7069
Luis <luiscastro250302@gmail.com>
2024-09-20 16:07:13 -0300

    Renamed folder from 'test' to 'tests'. Some tests of the Interface class.

---
+++ tests/test_interface.py
@@ -0,0 +1,20 @@
+import pytest
+from main import Interface
+from habit_tracker import Habit
+
+
+@pytest.fixture
+def interface():
+    return Interface()
+
+def test_habit_added(interface):
+    errors = []
+
+    num_habits = len(interface.habit_tracker.habits)
+    interface.add_habit()
+    if len(interface.habit_tracker.habits) != num_habits + 1:
+        errors.append("No habit was stored!")
+    if not isinstance(interface.habit_tracker.habits[-1], Habit):
+        errors.append("The object stored is not a Habit!")
+
+    assert not errors
